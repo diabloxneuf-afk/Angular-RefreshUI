e638999d34fdc5b61f194aa7a8fde3d27d2bd282
Kevin Messier <kevin.messier@etu.cegepdrummond.ca>
2026-01-21 14:27:56 -0500

    programme <Exercice 2

--- src/app/exemple2/exemple2/exemple2.ts
+++ src/app/exemple2/exemple2/exemple2.ts
@@ -1,31 +1,44 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { MessageDto } from '../../dtos/message.dto';
 import { FormsModule } from '@angular/forms';
 import { Service2 } from '../service2/service2';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-exemple2',
   imports: [FormsModule],
   templateUrl: './exemple2.html',
   styleUrl: './exemple2.scss',
-  providers: [Service2]
+  providers: [],
 })
-export class Exemple2 {
-    newMessage: string = "";
-    messages: MessageDto[] = [];
+export class Exemple2 implements OnInit {
+  newMessage: string = '';
+  messages: MessageDto[] = [];
 
-  constructor(private service: Service2) {}
+  constructor(
+    private service: Service2,
+    private cdr: ChangeDetectorRef,
+  ) {}
 
-  addMessage() {
+  async ngOnInit() {
+    await this.getMessages();
+  }
+
+  async addMessage() {
     if (this.newMessage.trim().length > 0) {
-      const id = (Math.random() * 1000000).toFixed(0);
-      this.messages.push(({ id: id, content: this.newMessage.trim() }));
-      this.newMessage = "";
+      await firstValueFrom(this.service.addMessage({ content: this.newMessage.trim() }));
+      await this.getMessages();
+      this.newMessage = '';
     }
   }
 
-  removeMessage(id: string) {
-    this.messages = this.messages.filter(msg => msg.id !== id);
-    console.log(this.messages);
+  async getMessages() {
+    this.messages = await firstValueFrom(this.service.getMessages());
+    this.cdr.markForCheck(); //refresh le ui
+  }
+
+  async removeMessage(id: string) {
+    await firstValueFrom(this.service.deleteMessage(id));
+    await this.getMessages();
   }
 }
--- src/app/exemple2/service2/service2.ts
+++ src/app/exemple2/service2/service2.ts
@@ -1,11 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, Inject, Injectable } from '@angular/core';
+import { MessageDto } from '../../dtos/message.dto';
+import { HttpClient } from '@angular/common/http';
+import { map, Observable, switchMap } from 'rxjs';
 
-@Component({
-  selector: 'app-service2',
-  imports: [],
-  templateUrl: './service2.html',
-  styleUrl: './service2.scss',
+@Injectable({
+  providedIn: 'root',
 })
 export class Service2 {
+  url = 'https://my-new-project-e3774-default-rtdb.firebaseio.com/messages.json';
 
+  constructor(private http: HttpClient) {}
+
+  addMessage(message: MessageDto): Observable<void> {
+    return this.http.post<void>(this.url, message);
+  }
+
+  getMessages(): Observable<MessageDto[]> {
+    console.log(this.http.get<MessageDto[]>(this.url));
+
+    return this.http.get<MessageDto[]>(this.url).pipe(
+      map((data) => {
+        if (!data) {
+          return [];
+        }
+
+        //ceci va retriev la key de chaque élément de array et le mettre dans id
+        //retourne la liste complete avec id et content
+        return Object.entries(data).map(([key, value]) => ({
+          ...value,
+          id: key,
+        }));
+
+        //ne marche pas comme il faut, car je veux retriev la key de l'object our la mettre dans id
+        /*
+        const messages = Object.values(data); //transforme un object en tableau
+        console.log(data)
+        messages.forEach((msg) => {
+          console.log(msg.content);
+          console.log(msg.id);
+        });
+
+        return messages;
+        */
+      }),
+    );
+  }
+
+  deleteMessage(id: string): Observable<void> {
+    const deleteUrl = `https://my-new-project-e3774-default-rtdb.firebaseio.com/messages/${id}.json`;
+
+    return this.http.delete<void>(deleteUrl);
+  }
 }
